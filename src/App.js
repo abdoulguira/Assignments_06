7a2d9cdd8305043eae72fdbf5d626e7d849863e2
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-12 20:22:04 -0500

    added code for the index.html, stylesheet.css, App.js, and new components Header.js, Inputbox.js created. base-structure for assignment6 project1 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from './Header';
+import Inputbox from './Inputbox';
+
 
 export default function App() {
   return (
     <div>
+
+      <Header text="Zip Code Search" color="red" />
+      <Inputbox />
+
 
     </div>
   );
